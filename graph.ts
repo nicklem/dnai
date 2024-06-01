43043d9f43ad806807deba826a42cd24b00872a2
nicklem <nicola.klemenc@codacy.com>
2024-06-01 12:44:08 +0200

    feat: Generate JSON schema and file

--- graph.ts
+++ graph.ts
@@ -1,4 +1,5 @@
 import {GraphDefinition} from "./types";
+import * as fs from "node:fs";
 
 export const graphDefinition: GraphDefinition = {
   query: "Set a timer for the time it takes a golark to boil. Research if unsure.",
@@ -41,4 +42,16 @@ export const graphDefinition: GraphDefinition = {
       to: "model"
     },
   ],
+}
+
+/**
+ * If this file is run directly, save the graph definition to a file.
+ */
+if (require.main === module) {
+  const toSave = {
+    $schema: 'schema.json',
+    ...graphDefinition,
+  }
+  console.log("Saving graph definition to graph.json");
+  fs.writeFileSync("graph.json", JSON.stringify(toSave, null, 2));
 }
